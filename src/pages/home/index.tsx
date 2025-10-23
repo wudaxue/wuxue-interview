1201d169690bacd54a8f02653c5c866e10c72865
yang <1203588983@qq.com>
2025-10-24 03:10:14 +0800

    update route

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -2,11 +2,27 @@ import { Link } from 'react-router-dom'
 
 function Home() {
   return (
-    <div className='p-2 flex flex-col gap-2'>
-      <a className='hover:cursor-pointer underline text-blue-600' href='src/pages/ui/index.html' target="_blank">题目1 -- 静态UI</a>
-      <Link className='hover:cursor-pointer underline text-blue-600' to='/fetch'>题目2 -- 异步请求</Link>
-      <a className='hover:cursor-pointer underline text-blue-600' href='src/pages/message/main/index.html'>题目3 -- iframe通信</a>
-      <Link className='hover:cursor-pointer underline text-blue-600' to='/download'>题目4 -- 文件下载</Link>
+    <div className='flex flex-col gap-2 p-2'>
+      <a
+        className='text-blue-600 underline hover:cursor-pointer'
+        href='src/pages/ui/index.html'
+        target='_blank'
+        rel='noopener'
+      >
+        题目1 -- 静态UI
+      </a>
+      <Link className='text-blue-600 underline hover:cursor-pointer' to='/fetch'>
+        题目2 -- 异步请求
+      </Link>
+      <a
+        className='text-blue-600 underline hover:cursor-pointer'
+        href='src/pages/message/main/index.html'
+      >
+        题目3 -- iframe通信
+      </a>
+      <Link className='text-blue-600 underline hover:cursor-pointer' to='/download'>
+        题目4 -- 文件下载
+      </Link>
     </div>
   )
 }
--- vite.config.ts
+++ vite.config.ts
@@ -12,8 +12,9 @@ export default defineConfig({
       input: {
         main: resolve(__dirname, 'index.html'),
         messageMain: resolve(__dirname, `src/pages/message/main/index.html`),
-        messageIframe1: resolve(__dirname, `src/pages/message/frame1/index.html`),
-        messageIframe2: resolve(__dirname, `src/pages/message/frame2/index.html`),
+        messageIframe1: resolve(__dirname, `src/pages/message/iframe1/index.html`),
+        messageIframe2: resolve(__dirname, `src/pages/message/iframe2/index.html`),
+        messageIframe3: resolve(__dirname, `src/pages/message/iframe3/index.html`),
         ui: resolve(__dirname, `src/pages/ui/index.html`),
       },
     },
